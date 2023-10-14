69251c615f9ccadff5b762dd373b141a764cb2f0
imtiazimran <imtiazimran@outlook.com>
2023-10-14 23:33:36 +0600

    details polished

--- src/Components/AcountenenDetails/AcountenenDetails.jsx
+++ src/Components/AcountenenDetails/AcountenenDetails.jsx
@@ -9,9 +9,9 @@ const AccountantDetails = () => {
     const targetedData = loaderData.find(item => item._id === Id)
 
     return (
-        <div className="flex w-4/5 mx-auto">
+        <div className="flex flex-col md:flex-row md:w-4/5 mx-auto">
             {/*-------------------------------- Left-side content------------------------------------ */}
-            <div className="w-1/2 p-4">
+            <div className="md:w-1/2 p-4">
                 <div className="bg-white rounded shadow p-4">
                     <h2 className="text-2xl font-semibold">{targetedData.name}</h2>
                     <p className="text-gray-600 mt-2">{targetedData.intro}</p>
@@ -25,12 +25,12 @@ const AccountantDetails = () => {
                 <div className="bg-white rounded shadow p-4 mt-4">
                     {/* taskComplexity content */}
                     <div>
-                        <div className='flex justify-between'>
+                        <div className='flex flex-col md:flex-row justify-between'>
                             <p>{targetedData.taskComplexity}</p>
                             <p className='font-bold'>{targetedData.price}</p>
                         </div>
                         <p className='flex my-3'> <img className='w-7' src="https://logowik.com/content/uploads/images/calendar9737.jpg" alt="" />{targetedData.deliveryTime}</p>
-                        <div className='flex gap-5 font-bold text-xl justify-center'>
+                        <div className='flex flex-col md:flex-row gap-5 font-bold text-xl justify-center'>
                             <button className="btn rounded-md btn-active bg-[#0076CE] text-white hover:bg-blue-800">Requist Proposel</button>
                             <button className="btn rounded-md btn-outline border-[#0076CE] hover:bg-[#0076CE]">Chat With me</button>
                         </div>
@@ -49,12 +49,12 @@ const AccountantDetails = () => {
 
             {/*----------------------------- Right-side content----------------------------------------- */}
 
-            <div className="w-1/2 p-4">
+            <div className="md:w-1/2 p-4">
                 <img src={targetedData.image} alt={targetedData.name} className="rounded shadow" />
                 {/* About */}
                 <div className="bg-white rounded shadow p-4 mt-4">
                     <h2 className="text-[35px] py-[22px] font-semibold text-center">About {targetedData.name}</h2>
-                    <div className='flex justify-between text-center'>
+                    <div className='flex flex-col md:flex-row justify-between text-center'>
                         <div>
                             <p className="text-[#999999] text-[16px] font-bold mt-2">From</p>
                             <p>{targetedData.about.from}</p>
@@ -73,7 +73,7 @@ const AccountantDetails = () => {
                 </div>
 
                 {/* Services and Why Me */}
-                <div className="bg-white rounded shadow p-4 mt-4 flex justify-between">
+                <div className="bg-white rounded shadow p-4 mt-4 flex flex-col- md:flex-row justify-between">
                     <div>
                         <h2 className="text-[#999999] text-[16px] font-bold">Services I Offer</h2>
                         {
